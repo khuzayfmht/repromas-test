f836e0f366463ac9410102c094f618d326180f38
ISA ALHAJI MUHAMMED <isa.muhammad@flexisaf.com>
2024-07-07 09:48:00 +0100

    Student Card

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,11 @@
+
 const Dashboard = () => {
     return (
-        <div>Welcome to react router dome</div>
+        <div style={{ display: "grid", gridTemplateColumns: '1fr 1fr 1fr 1fr'}}>
+            <div style={{ borderRadius: '5px', background: '#F3C5FF', height: '100px'}}>
+                <h3 style={{color: '#4B4453'}}>Student</h3>
+            </div>
+        </div>
     )
 }
 
