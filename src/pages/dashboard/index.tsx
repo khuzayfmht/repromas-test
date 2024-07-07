fe5d79f31b7aa9f22c9522e5bd6da69a4c09f19a
ISA ALHAJI MUHAMMED <isa.muhammad@flexisaf.com>
2024-07-07 21:12:00 +0100

    Some chnages

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,7 +3,7 @@ const Dashboard = () => {
     return (
         <div style={{ display: "grid", gridTemplateColumns: '1fr 1fr 1fr 1fr'}}>
             <div style={{ borderRadius: '5px', background: '#F3C5FF', height: '100px'}}>
-                <h3 style={{color: '#4B4453'}}>Student</h3>
+                <h3 style={{color: '#4B4453'}}>Nasir</h3>
             </div>
         </div>
     )
