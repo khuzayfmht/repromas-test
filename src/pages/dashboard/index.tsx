d386bf4217b933db4244c26db121e3245b7706a6
ISA ALHAJI MUHAMMED <isa.muhammad@flexisaf.com>
2024-07-09 22:13:35 +0100

    Add some card

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,11 +1,23 @@
 
 const Dashboard = () => {
     return (
-        <div style={{ display: "grid", gridTemplateColumns: '1fr 1fr 1fr 1fr'}}>
+        <>
+            <h3>Team Members</h3>
+            <div style={{ display: "grid", gridTemplateColumns: '1fr 1fr 1fr 1fr', padding: '20px' }}>
             <div style={{ borderRadius: '5px', background: '#F3C5FF', height: '100px'}}>
                 <h3 style={{color: '#4B4453'}}>Nasir</h3>
             </div>
+            <div style={{ borderRadius: '5px', background: '#F9B14E', height: '100px'}}>
+                <h3 style={{color: '#4B4453'}}>Khuzaifa (Team Lead)</h3>
+            </div>
+            <div style={{ borderRadius: '5px', background: '#C4FCF0', height: '100px'}}>
+                <h3 style={{color: '#4B4453'}}>Zunnurain</h3>
+            </div>
+            <div style={{ borderRadius: '5px', background: '#80B2C9', height: '100px'}}>
+                <h3 style={{color: '#4B4453'}}>ISa</h3>
+            </div>
         </div>
+        </>
     )
 }
 
