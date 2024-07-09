835ee92f823e0b9cd9f76f583d7ffbdbf37b3888
ISA ALHAJI MUHAMMED <isa.muhammad@flexisaf.com>
2024-07-09 22:17:19 +0100

    add gap to dasboard card

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,7 +3,7 @@ const Dashboard = () => {
     return (
         <>
             <h3>Team Members</h3>
-            <div style={{ display: "grid", gridTemplateColumns: '1fr 1fr 1fr 1fr', padding: '20px' }}>
+            <div style={{ display: "grid", gridTemplateColumns: '1fr 1fr 1fr 1fr', gridGap: '10px', padding: '20px' }}>
             <div style={{ borderRadius: '5px', background: '#F3C5FF', height: '100px'}}>
                 <h3 style={{color: '#4B4453'}}>Nasir</h3>
             </div>
@@ -14,7 +14,7 @@ const Dashboard = () => {
                 <h3 style={{color: '#4B4453'}}>Zunnurain</h3>
             </div>
             <div style={{ borderRadius: '5px', background: '#80B2C9', height: '100px'}}>
-                <h3 style={{color: '#4B4453'}}>ISa</h3>
+                <h3 style={{color: '#4B4453'}}>Isa</h3>
             </div>
         </div>
         </>
